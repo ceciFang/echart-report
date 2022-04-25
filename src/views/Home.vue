375fb5fae64d0f7f62584270a5ebc207185b0470
Ceci Fang <1298397370@qq.com>
2022-04-25 22:41:49 +0800

    第一次

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <top-view/>
+    <sales-view/>
+    <bottom-view/>
+    <map-view/>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import BottomView from '../components/BottomView/index.vue'
+import mapView from '../components/mapView/index.vue'
+import SalesView from '../components/SalesView/index.vue'
+import TopView from '../components/TopView/inde.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TopView,
+    SalesView,
+    mapView,
+    BottomView
   }
 }
 </script>
+<style>
+.home{
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background: #eee;
+}
+
+</style>
